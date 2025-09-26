f47c6fb5e37bac2af99bcac1ebd13450baa367b1
Dimitri <dimitri.gomes@grupodr.com.br>
2025-09-26 09:47:04 -0300

    fix redrect uri

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -5,7 +5,7 @@ export const msalConfig: Configuration = {
     clientId: "0db3ca77-2cf5-4ef3-a115-ac5e76db0bec", // pegue no portal Azure
     authority:
       "https://login.microsoftonline.com/fa2cefb2-831d-4bde-b4b9-09d384a84ffd", // ou seu tenant específico
-    redirectUri: "http://localhost:5173", // porta padrão do Vite
+    redirectUri: "https://cardapio-interface.vercel.app/", // porta padrão do Vite
   },
   cache: {
     cacheLocation: "localStorage",
